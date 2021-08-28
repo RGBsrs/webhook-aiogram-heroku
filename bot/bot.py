0fc5029e2f1b625e2c14d477685a290b15b8c69e
Dmytro <grjanko.d.a@gmail.com>
2021-08-28 16:54:52 +0300

    add 4pda parsing handler

--- bot/bot.py
+++ bot/bot.py
@@ -4,20 +4,18 @@ from aiogram import Bot, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
-from bot.settings import (BOT_TOKEN, HEROKU_APP_NAME,
-                          WEBHOOK_URL, WEBHOOK_PATH,
-                          WEBAPP_HOST, WEBAPP_PORT)
+from bot.settings import *
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
 
-@dp.message_handler()
+@dp.message_handler(commands=['4pda'])
 async def echo(message: types.Message):
-    logging.warning(f'Recieved a message from {message.from_user}')
-    await bot.send_message(message.chat.id, message.text)
+    # logging.warning(f'Recieved a message from {message.from_user}')
 
+    await message.reply("parsing 4pda")
 
 async def on_startup(dp):
     logging.warning(
