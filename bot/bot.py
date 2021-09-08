fda95198a4a624566e34f8a4da5853ee9933ad3a
Dmytro <grjanko.d.a@gmail.com>
2021-09-08 10:51:10 +0300

    chage bot to echo

--- bot/bot.py
+++ bot/bot.py
@@ -14,21 +14,9 @@ pda_parser = PdaParser()
 habr_parser = HabrParser()
 
 
-@dp.message_handler(commands=['4pda'])
+@dp.message_handler(content_types=['text'])
 async def echo(message: types.Message):
-    resp = await pda_parser.get_response()
-    if resp.status_code == 200:
-        posts = await pda_parser.process_html()
-        logging.warning('make asnwer')
-        await message.answer(posts)
-
-@dp.message_handler(commands=['habr'])
-async def echo(message: types.Message):
-    resp = await habr_parser.get_response()
-    if resp.status_code == 200:
-        posts = await pda_parser.process_html()
-        logging.warning('make asnwer')
-        await message.answer(posts)
+    await message.answer(message.text)
 
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
